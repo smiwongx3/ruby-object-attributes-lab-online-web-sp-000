5e7208ab0ff1fa03c72b8972effb9959ac539514
Samantha Wong <smiwong920@gmail.com>
2019-02-15 23:24:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,27 @@
 class Dog 
-  
-  def breed(breed_name)
-    @breed = breed_name
-  end 
-  
-end 
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+ 
+  def name=(new_name)
+    @name = new_name
+  end
+ 
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end 
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+ 
+end
